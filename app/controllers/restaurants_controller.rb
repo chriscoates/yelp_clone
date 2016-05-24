2785863a4da2e176129cc96940a7a8c162e61f22
chriselevate <coates.c@hotmail.com>
2016-05-24 18:03:16 +0100

    13 testing validations complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,8 +9,12 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    @restaurant = Restaurant.create(restaurant_params)
-    redirect_to '/restaurants'
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render 'new'
+    end
   end
 
   def restaurant_params
@@ -22,20 +26,20 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
-     @restaurant = Restaurant.find(params[:id])
-  end
+   @restaurant = Restaurant.find(params[:id])
+ end
 
-  def update
-    @restaurant = Restaurant.find(params[:id])
-    @restaurant.update(restaurant_params)
-    redirect_to '/restaurants'
-  end
+ def update
+  @restaurant = Restaurant.find(params[:id])
+  @restaurant.update(restaurant_params)
+  redirect_to '/restaurants'
+end
 
-  def destroy
-    @restaurant = Restaurant.find(params[:id])
-    @restaurant.destroy
-    flash[:notice] = 'Restaurant deleted successfully'
-    redirect_to '/restaurants'
-  end
+def destroy
+  @restaurant = Restaurant.find(params[:id])
+  @restaurant.destroy
+  flash[:notice] = 'Restaurant deleted successfully'
+  redirect_to '/restaurants'
+end
 
 end

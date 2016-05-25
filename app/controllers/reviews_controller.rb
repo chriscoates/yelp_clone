8e6152a0bf0d637fa53c755b1469510ee889cd8a
letianw <tmac4ever0824@gmail.com>
2016-05-26 02:39:46 +1000

    opening #3-3 buildwithuser something something

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,21 +5,30 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
-  def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.reviews.new(review_params)
-    if @review.save
-      redirect_to '/restaurants'
-    else
-      render 'new'
-    end
+  # def create
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  #   @review = @restaurant.reviews.new(review_params)
+  #   if @review.save
+  #     redirect_to '/restaurants'
+  #   else
+  #     render 'new'
+  #   end
 
-  end
+  # end
 
   def review_params
     params.require(:review).permit(:thoughts, :rating)
   end
 
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.build_review review_params, current_user
+    if @review.save
+      redirect_to restaurants_path
+    else
+      render 'new'
+    end
+  end
 
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,15 @@
 class Restaurant < ActiveRecord::Base
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy do
+    def build_with_user(attributes = {}, user)
+      attributes[:user] ||= user
+      build(attributes)
+    end
+  end
+
   validates :name, length: { minimum: 3 }, uniqueness: true
 
+  def build_review(attributes, current_user)
+    attributes[:user] ||= current_user
+    reviews.build(attributes)
+  end
 end
